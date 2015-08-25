f4d6b8335394efbb8ea194072ad04b3305f06134
John McCormack <mccormack.wgsi@gmail.com>
2015-08-25 18:08:05 -0400

    Moving through Section 3.1

--- chapter3/guessing_game/src/main.rs
+++ chapter3/guessing_game/src/main.rs
@@ -1,14 +1,22 @@
+extern crate rand;
+
 use std::io;
+use rand::Rng;
 
-fn main() {
-    println!("Guess the number!");
-    println!("Please input your guess.");
+fn main(){
+	println!("Guess the number!");
+	
+	let secret_number = rand::thread_rng().gen_range(1, 101);
 
-    let mut guess = String :: new();
+	println!("The secret number is: {}", secret_number);
 
-    io::stdin().read_line(&mut guess)
-        .ok()
-        .expect("Failed to read line");
+	println!("Please input your guess.");
 
-    println!("You guessed: {}", guess);
+	let mut guess = String::new();
+
+	io::stdin().read_line(&mut guess)
+		.ok()
+		.expect("Failed to read line");
+
+	println!("You guessed: {}", guess);
 }
