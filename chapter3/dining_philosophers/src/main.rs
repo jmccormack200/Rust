5fba6de3c7526bea7db464133833b3c5f9f78f0e
John McCormack <mccormack.wgsi@gmail.com>
2015-08-25 21:46:51 -0400

    Started Section 3.2

---
+++ chapter3/dining_philosophers/src/main.rs
@@ -0,0 +1,31 @@
+struct Philosopher {
+	name: String,
+}
+
+impl Philosopher{
+	fn new(name: &str) -> Philosopher {
+		Philosopher{
+			name: name.to_string(),
+		}
+	}
+	
+	fn eat(&self){
+		println!("{} is done eating.", self.name);
+	}
+}
+
+fn main() {
+	let philosophers = vec! [
+		Philosopher::new("Judith Butler"),
+		Philosopher::new("Gilles Deleuze"),
+		Philosopher::new("Karl Marx"),
+		Philosopher::new("Emma Goldman"),
+		Philosopher::new("Michel Foucult"),
+	];
+
+	for p in &philosophers{
+		p.eat();
+	}
+}
+
+
