9fec1e9b16849bd950aa0f527d85795dd8d5bf3c
John McCormack <mccormack.wgsi@gmail.com>
2015-08-25 22:04:59 -0400

    Finished section 3.2

--- chapter3/dining_philosophers/src/main.rs
+++ chapter3/dining_philosophers/src/main.rs
@@ -1,17 +1,29 @@
 use std::thread;
+use std::sync::{Mutex, Arc};
+
+struct Table{
+	forks: Vec<Mutex<()>>,
+}
 
 struct Philosopher {
 	name: String,
+	left: usize,
+	right: usize,
 }
 
 impl Philosopher{
-	fn new(name: &str) -> Philosopher {
+	fn new(name: &str, left:usize, right:usize) -> Philosopher {
 		Philosopher{
 			name: name.to_string(),
+			left: left,
+			right: right,
 		}
 	}
 	
-	fn eat(&self){
+	fn eat(&self, table: &Table){
+		let _left = table.forks[self.left].lock().unwrap();
+		let _right = table.forks[self.right].lock().unwrap();
+
 		println!("{} is eating.", self.name);
 
 		thread::sleep_ms(1000);
@@ -21,17 +33,27 @@ impl Philosopher{
 }
 
 fn main() {
+	let table = Arc::new(Table { forks: vec! [
+		Mutex::new(()),
+		Mutex::new(()),
+		Mutex::new(()),
+		Mutex::new(()),
+		Mutex::new(()),
+	]});
+
 	let philosophers = vec! [
-		Philosopher::new("Judith Butler"),
-		Philosopher::new("Gilles Deleuze"),
-		Philosopher::new("Karl Marx"),
-		Philosopher::new("Emma Goldman"),
-		Philosopher::new("Michel Foucult"),
+		Philosopher::new("Judith Butler", 0, 1),
+		Philosopher::new("Gilles Deleuze", 1, 2),
+		Philosopher::new("Karl Marx",2,3),
+		Philosopher::new("Emma Goldman",3,4),
+		Philosopher::new("Michel Foucult",0,4),
 	];
 	
 	let handles: Vec<_> = philosophers.into_iter().map(|p| {
+		let table = table.clone();
+
 		thread::spawn(move || {
-			p.eat();	
+			p.eat(&table);	
 		})
 	}).collect();
 
