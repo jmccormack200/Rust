76bb3406b02446e2dbf47a0c1ff925ed43214852
John McCormack <mccormack.wgsi@gmail.com>
2015-08-28 10:30:04 -0400

    Finished section on documentation

---
+++ chapter4/docs/src/lib.rs
@@ -0,0 +1,11 @@
+/// Constructs a new 'Rc <T>'.
+///
+/// # Examples
+///
+/// '''
+/// use std::rc::Rc;
+/// let five = RC::new(5);
+/// '''
+pub fn new(value: T) -> Rc<T>{
+	// implementation goes here
+}
